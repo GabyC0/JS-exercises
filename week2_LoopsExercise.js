8fd8f5849663ef35bba345673378044b4eb1a1a4
Gaby Campos <gabycamposcontact@gmail.com>
2021-12-27 13:39:12 -0800

    Week 2 Loops Exercise

--- week2_LoopsExercise.js
+++ week2_LoopsExercise.js
@@ -12,103 +12,98 @@
 // Exercise 1. Write a 'while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
 
-// let int = 1;
-// while (int <= 5) {
-//   console.log(int);
-//   int++;
-// }
+let int = 1;
+while (int <= 5) {
+  console.log(int);
+  int++;
+}
 
 // Exercise 2. Write a 'do while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
-// let i = 0;
-// do {
-//   i ++;
-//   console.log(i);
-// } while (i < 5);
+
+let i = 0;
+do {
+  i ++;
+  console.log(i);
+} while (i < 5);
 
 
 // Exercise 3. Write a 'for' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
 
-// for (let num = 1; num <=5; num++) {
-//   console.log(num);
-// }
+for (let num = 1; num <=5; num++) {
+  console.log(num);
+}
 
 // Exercise 4. Now we want a loop that prints the integers
 // counting DOWN from 10 to 1 (inclusive). Write three loops that do this, to practice
 // writing loops in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
 //while loop
-// let int = 10;
-// while (int > 0) {
-//   console.log(int);
-//   int--;
-// }
+let int = 10;
+while (int > 0) {
+  console.log(int);
+  int--;
+}
 
 
 //do while loop
-// let int = 10;
-// do {
-//   console.log(int);
-//   int--;
-// } while (int > 0);
+let int = 10;
+do {
+  console.log(int);
+  int--;
+} while (int > 0);
 
 //for loop
-// for (let int = 10; int > 0; int--) {
-//   console.log(int);
-// }
+for (let int = 10; int > 0; int--) {
+  console.log(int);
+}
 
 // Exercise 5. Write a loop that prints the integers from 7 to 27. Write this
 // loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
-//while
 
-// let int = 7;
-// while (int <= 27) {
-//   console.log(int);
-//   int++;
-// }
+//while
+let int = 7;
+while (int <= 27) {
+  console.log(int);
+  int++;
+}
 
 //do while
-
-// let int = 7;
-// do {
-//   console.log(int);
-//   int++;
-// } while (int <= 27);
+let int = 7;
+do {
+  console.log(int);
+  int++;
+} while (int <= 27);
 
 
 //for
-// for (let int = 7; int <= 27; int++) {
-//   console.log(int);
-// }
+for (let int = 7; int <= 27; int++) {
+  console.log(int);
+}
 
 // Exercise 6. Write a loop that prints numbers between 0 and 100, counting by tens.
 // I.e. it will print 10, then 20, then 30, etc.
 // Write this loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
 //while
-
-// let countByTen = 10;
-// while (countByTen <= 100) {
-//   console.log(countByTen);
-//   countByTen += 10;
-// }
+let countByTen = 10;
+while (countByTen <= 100) {
+  console.log(countByTen);
+  countByTen += 10;
+}
 
 //do while
-
-// let countByTen = 10;
-// do {
-//   console.log(countByTen);
-//   countByTen += 10;
-// } while (countByTen <= 100);
+let countByTen = 10;
+do {
+  console.log(countByTen);
+  countByTen += 10;
+} while (countByTen <= 100);
 
 //for
-
-// for(let num = 10; num <= 100; num += 10) {
-//   console.log(num);
-// }
-
-
+for(let num = 10; num <= 100; num += 10) {
+  console.log(num);
+}
 
 
 
@@ -122,23 +117,24 @@
 //   counterFour--;
 // }
 
-// let counterFour = 1;
-// while (counterFour >= -100) {
-//   console.log(counterFour);
-//   counterFour--;
-// }
+let counterFour = 1;
+while (counterFour >= -100) {
+  console.log(counterFour);
+  counterFour--;
+}
+
 //Response
 //The original loop is an infinite loop because counterFour is decrementing as long as it meets the condition of counterFour being less than 2. It will always be less than 2 as long as it's decrementing from 1.
 
 // Exercise 8. Make a variable that contains your favorite integer. Write a loop
 // (your choice which type) that prints the integers from 0 to that number.
 
-// let myFavNum = 7;
-// let i = 0;
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= myFavNum);
+let myFavNum = 7;
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= myFavNum);
 
 // for (let i = 0; i <= myFavNum; i++) {
 //   console.log(i);
@@ -155,15 +151,15 @@
 // ...
 // (Hint - use an if statement in your loop)
 
-// let myFavNum = 50;
+let myFavNum = 50;
 
-// for (let randoNum = 0; randoNum <= 100; randoNum++) {
-//   if(randoNum === myFavNum) {
-//     console.log(myFavNum + " my favorite number!")
-//   } else {
-//     console.log(randoNum + " not my favorite number")
-//   }
-// }
+for (let randoNum = 0; randoNum <= 100; randoNum++) {
+  if(randoNum === myFavNum) {
+    console.log(myFavNum + " my favorite number!")
+  } else {
+    console.log(randoNum + " not my favorite number")
+  }
+}
 
 // Exercise 10. In some of the exercises above, we had you write all 3 types of loops, for practice.
 // But in real life, how would you decide which type of loop to use? You might not have an
